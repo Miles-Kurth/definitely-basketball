156c7fc75fb3fc1e425460a878f76e62074c6de4
Miles-Kurth <mkurth9612@student.pps.net>
2026-04-10 15:00:51 -0700

    hmm

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, GyroSensor, ColorSensor, TouchSensor
+from pybricks.parameters import Port,Stop
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+from pybricks.iodevices import I2CDevice
+import time
+import sys
+
+
+class LaserSensor:
+    def __init__(self, port):
+        self.i2c = I2CDevice(port, 0x02 >> 1)
+        self.last_time = 0
+        self.last_dist = 0
+
+    def distance(self):
+        now = time.time()
+        if now - self.last_time > 0.03:
+            self.last_time = now
+            results = self.i2c.read(0x42, 2)
+            self.last_dist = results[0] + (results[1] << 8)
+        return self.last_dist
+
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize motors
+yeet_motor = Motor(Port.A)
+
+yeet_motor.run(1000)
+
+wait(3000)
+
+yeet_motor.brake()
+
+
+
 
