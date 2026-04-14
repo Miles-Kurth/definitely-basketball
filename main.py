555aad2cbb94be10d9796b0a90c24ab483cd7846
Miles-Kurth <mkurth9612@student.pps.net>
2026-04-14 15:26:43 -0700

    hmm working. button yes is yes

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ ev3 = EV3Brick()
 # Initialize motors
 yeet_motor = Motor(Port.A)
 
+# Initialize sensors
+button = TouchSensor(Port.S1)
+
 
 # At start
 ev3.speaker.set_volume(40); #ev3.speaker.beep(660,200)
@@ -39,11 +42,18 @@ ev3.speaker.beep(440)
 
 # CODE BELOW
 
-yeet_motor.run(1000)
+isRunning = False
 
-wait(3000)
 
-yeet_motor.brake()
+while True:
+    if (button.pressed()):
+        if (isRunning == False):
+            yeet_motor.dc(100)
+        isRunning = True
+    else:
+        if (isRunning == True):
+            yeet_motor.brake()
+        isRunning = False
 
 
 
