1f7e016e9f9803578d6cc0d5c4090762cb94d864
Miles-Kurth <mkurth9612@student.pps.net>
2026-04-24 15:31:25 -0700

    switching to a video game

--- main.py
+++ main.py
@@ -37,10 +37,11 @@ motor2 = Motor(Port.B)
 button = TouchSensor(Port.S1)
 
 
-# At start
-ev3.speaker.set_volume(40); #ev3.speaker.beep(660,200)
-ev3.speaker.beep(440)
 
+SCREENWIDTH = 177
+SCREENHEIGHT = 127
+PLAYBOUNDSX = SCREENWIDTH - 1
+PLAYBOUNDSY = SCREENHEIGHT - 1
 
 speed = 7
 
@@ -52,26 +53,86 @@ def brake():
     motor1.brake()
     motor2.brake()
 
+def fan():
+    while True:
+        if (speed > 100):
+            speed = 100
+        print(speed)
+
+        run()
+
+        if (button.pressed()):
+            speed += 0.03 + (speed *0.001)
+        else:
+            speed += -0.2
+            if (speed <= 7):
+                speed = 7
+                brake()    
+
+
+
+def clearScreen():
+    ev3.screen.clear()
+
+
+def touchingLeft(x,y):
+    if (x == 1):
+        return True
+    return False
+
+def touchingRight(x,y):
+    if (x == PLAYBOUNDSX):
+        return True
+    return False
+
+def touchingTop(x,y):
+    if (y == 1):
+        return True
+    return False
+
+def touchingBottom(x,y):
+    if (y == PLAYBOUNDSY):
+        return True
+    return False
+
+def touchingH(x,y):
+    if (touchingLeft(x,y) or touchingRight(x,y)):
+        return True
+    return False
+
+def touchingV(x,y):
+    if (touchingTop(x,y) or touchingBottom(x,y)):
+        return True
+    return False
+
+def touchingEdge(x,y):
+    if (touchingH(x,y) or touchingV(x,y)):
+        return True
+    return False
+
+
+# At start
+ev3.speaker.set_volume(40); #ev3.speaker.beep(660,200)
+ev3.speaker.beep(440)
+
 
 # CODE BELOW
 
-isRunning = False
 
+clearScreen()
+for y in range(PLAYBOUNDSY):
+    for x in range(PLAYBOUNDSX):
+        ev3.screen.draw_pixel(x,y)
+
+clearScreen()
+
+ev3.screen.draw_box(1,1,SCREENWIDTH,SCREENHEIGHT,0,False)
 
 while True:
-    if (speed > 100):
-        speed = 100
-    print(speed)
+    pass
 
-    run()
 
-    if (button.pressed()):
-        speed += 0.03 + (speed *0.001)
-    else:
-        speed += -0.2
-        if (speed <= 7):
-            speed = 7
-            brake()        
+    
 
 
 
